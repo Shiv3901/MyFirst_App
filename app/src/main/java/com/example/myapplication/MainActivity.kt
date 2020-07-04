4399edc1ed6561b501cff4b7c95be9c5dc2231b4
Shiv3901 <shivambsavani@gmail.com>
2020-07-04 22:54:43 +0930

    Added some code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText( context: this, "Hello from Android!", Toast.LENGTH_LONG).show()
     }
 }
